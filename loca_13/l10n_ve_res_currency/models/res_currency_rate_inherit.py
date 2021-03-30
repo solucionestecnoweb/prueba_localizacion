5ac7ad08fc32c71de39484fb1f4155c1a4298a90
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2021-03-30 11:03:42 -0400

    Modulo moneda con precio $ en cada producto

--- loca_13/l10n_ve_res_currency/models/res_currency_rate_inherit.py
+++ loca_13/l10n_ve_res_currency/models/res_currency_rate_inherit.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api,_
 from datetime import timedelta, date, datetime
+from odoo.exceptions import UserError
 #Moneda..
 class CurrencyRate(models.Model):
     _inherit = "res.currency.rate"
@@ -9,7 +10,7 @@ class CurrencyRate(models.Model):
     hora = fields.Datetime('Fecha y Hora', default=lambda self: fields.datetime.today(), required=True)
     #name = fields.Datetime('Fecha y Hora', default=lambda self: fields.datetime.today(), required=True)
     rate_real = fields.Float(digits=(12, 2), help='se introduce la tasa real del mercado')
-    rate = fields.Float(digits=(12, 9), help='The rate of the currency to the currency of rate 1')
+    rate = fields.Float(digits=(12, 20), help='The rate of the currency to the currency of rate 1')
     _sql_constraints = [('unique_name_per_day', 'CHECK(1=1)', 'Only one currency rate per day allowed!')]
 
     @api.onchange('rate_real', 'hora')
@@ -19,19 +20,28 @@ class CurrencyRate(models.Model):
         if self.rate_real:
             tasa_real=self.rate_real
         rate = (1 /tasa_real)
+        #raise UserError(_("Rate=%s")%rate)
         self.rate = rate
         lista_tasa = self.env['res.currency.rate'].search([('name','=',hoy)],order='id asc')
         for det in lista_tasa:
             lista_tasa.write({
                 'name': (datetime.now() - timedelta(days=(1))),
                 })
+        self.convercion_precio_product()
         #self.name=datetime.now() - timedelta(days=(1))
+
+    def convercion_precio_product(self):
+        lista_product = self.env['product.template'].search([('moneda_divisa_venta', '=', self.currency_id.id)],order='id asc')
+        if lista_product:
+            for cor in lista_product:
+                precio=cor.list_price2*self.rate_real
+                cor.list_price=precio
 
 class Currency(models.Model):
     _inherit = "res.currency"
 
     rate_real = fields.Float(compute='_compute_tasa_real', digits=(12, 2), help='se introduce la tasa real del mercado')
-    rate = fields.Float(compute='_compute_current_rate', string='Current Rate', digits=(12, 9),
+    rate = fields.Float(compute='_compute_current_rate', string='Current Rate', digits=(12, 20),
                         help='The rate of the currency to the currency of rate 1.')
     rate_rounding = fields.Float(digits=(12, 9), help='la tasa inversa del mercado')
 
@@ -47,3 +57,11 @@ class Currency(models.Model):
             tasa_actual_inv=1
         self.rate_real=tasa_actual
         self.rate=tasa_actual_inv
+        self.convercion_precio_product2()
+
+    def convercion_precio_product2(self):
+        lista_product = self.env['product.template'].search([('moneda_divisa_venta', '=', self.id)],order='id asc')
+        if lista_product:
+            for cor in lista_product:
+                precio=cor.list_price2*self.rate_real
+                cor.list_price=precio
